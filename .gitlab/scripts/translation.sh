96d1a6f7eb97817be57d72c83f1b72468eacebb4
ceohobo1986 <127355533+ceohobo1986@users.noreply.github.com>
2023-03-09 08:01:54 +0200

    Delete .gitlab directory

--- .gitlab/scripts/translation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-VELOREN_ASSETS="$(pwd)/assets"
-export VELOREN_ASSETS
-
-time cargo run --bin i18n-csv --features="stat"
--- .gitlab/scripts/unittest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VELOREN_ASSETS="$(pwd)/assets"
-export VELOREN_ASSETS
-
-time cargo test \
-    --package veloren-common-assets asset_tweak::tests \
-    --features asset_tweak --lib &&
-time cargo test
